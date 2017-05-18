3757aead4c23bd9832815194a0a3e912cc9f8bc9
Edwin Chen <edwinchen85@gmail.com>
2017-05-18 22:53:30 +0800

    Define variables

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 document.addEventListener('click', lightClick);
 
 function lightboxClick(event) {
-
+  var elem = event.target,
+      elemID = elem.getAttribute('id'),
+      lightboxImg = document.getElementById('lightbox-image'),
+      lightbox = document.getElementById('lightbox-overlay'),
+      newImg = new Image();
 }
